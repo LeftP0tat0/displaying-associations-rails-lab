f16b08d70954008f544f9928815667528a48307c
Vivian stanley <vivianstanley@Vivians-MacBook-Air.local>
2021-07-12 22:10:57 -0700

    commit tree 8c50b3c0577848a6c51974d3538457c71cbc9f42

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,6 +3,7 @@ class SongsController < ApplicationController
   end
 
   def show
+    find_song
   end
 
   def new
@@ -21,11 +22,11 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    find_song
   end
 
   def update
-    @song = Song.find(params[:id])
+    find_song
 
     @song.update(song_params)
 
@@ -37,7 +38,7 @@ class SongsController < ApplicationController
   end
 
   def destroy
-    @song = Song.find(params[:id])
+    find_song
     @song.destroy
     flash[:notice] = "Song deleted."
     redirect_to songs_path
@@ -51,5 +52,9 @@ class SongsController < ApplicationController
 
   def artist_params
     params.require(:artist).permit(:name)
+  end
+
+  def find_song
+    @song = Song.find(params[:id])
   end
 end
